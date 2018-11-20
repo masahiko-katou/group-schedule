f10f6aa46c01b6763284cda2e67eb0731c495018
masahiko.katou <masserhorn.952@gmail.com>
2018-11-20 05:32:25 +0000

    not add past schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,9 +3,15 @@ class Schedule < ApplicationRecord
   
   validates :user_id, presence: true, length: { maximum: 255 }
   validates :event, presence: true, length: {maximum: 50 }
+  validate :date_cannot_be_in_the_past
   validates :location, length: {maximum: 255}
   validates :detail, length: {maximum: 255}
   
   has_many :answers, dependent: :destroy
   
+  def date_cannot_be_in_the_past
+    if event_date.present? && event_date < Date.today
+      errors.add(:event_date, ": 過去の日付は使用できません")
+    end
+  end
 end
